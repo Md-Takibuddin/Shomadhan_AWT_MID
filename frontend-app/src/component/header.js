b690e5070c69045041c171874371c74b4e94ec8f
reasat-rafio <rafioar96@gmail.com>
2022-11-05 18:28:07 +0600

    :construction: dashboard header component added

---
+++ frontend-app/src/component/header.js
@@ -0,0 +1,32 @@
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = `
+             <nav class="flex">
+                    <h1 class="flex-1 | text-purple text-[38px] font-bold">
+                        Dashboard
+                    </h1>
+                    <div class="flex space-x-3">
+                        <div class="flex flex-col">
+                            <span class="text-xl font-semibold"
+                                >Md Takib Uddin</span
+                            >
+                            <span
+                                class="text-right text-light_gray font-medium text-sm"
+                                >Student</span
+                            >
+                        </div>
+                        <img
+                            class="h-[65px] w-[65px] rounded-full"
+                            src="../../../public/dp.png"
+                            alt=""
+                        />
+                    </div>
+                </nav>
+            `;
+    }
+}
+
+customElements.define("haeder-component", Header);
