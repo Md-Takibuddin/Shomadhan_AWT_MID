e50bfaf6f2ab9857abf00341d85e1c0132f39ba7
reasat-rafio <rafioar96@gmail.com>
2022-11-06 01:15:03 +0600

    :package: tailwind init at root

---
+++ frontend-app/src/dashboard/appointment/script.js
@@ -0,0 +1,11 @@
+l;
+const appointmentBtn = document.querySelector("#appointment_btn");
+console.log("====================================");
+console.log(appointmentBtn);
+console.log("====================================");
+appointmentBtn.addEventListener("click", () => {
+    console.log("====================================");
+    console.log("eee");
+    console.log("====================================");
+    appointmentBtn.style.display = "flex";
+});
