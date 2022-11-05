6a8c00efb191f8b8f53d3b290d8d36d8f3589932
reasat-rafio <rafioar96@gmail.com>
2022-11-05 15:02:55 +0600

    :package: tailwind init

---
+++ frontend-app/tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./scripts/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+
+// "./src/**/*.{html,js}",
