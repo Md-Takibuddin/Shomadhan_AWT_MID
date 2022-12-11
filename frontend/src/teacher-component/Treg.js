16b0e76793c6835155e96bbed983d67791d73d88
shohanurshihab <skshihab531@gmail.com>
2022-12-12 04:32:49 +0600

    Teacher-Reg

--- frontend/src/teacher-component/Treg.js
+++ frontend/src/teacher-component/Treg.js
@@ -1,3 +1,5 @@
+import "./tlog.css";
+
 
 const Treg = () => {
     return ( 
@@ -11,66 +13,67 @@ const Treg = () => {
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"/>
 </head>
 <body>
-<div class="box-form">
-	<div class="left">
-		<div class="overlay">
+    <div className="max">
+<div className="box-form">
+	<div className="left">
+		<div className="overlay">
 		<h1></h1>
-        <img src="#" alt="somadhan" class="logo"/>
+        <img src="storage\ui-photos\logo copy.png" alt="somadhan" className=""/>
 		<p>Somadhan is your best companion for study. Access answers to all the question you have. Live tutors to help you out. <br/>Join Today! </p>
 		<span>
 			<p>Visit our social media pages</p>
-			<a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-			<a href="https://www.twitter.com/"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a>
+			<a href="https://www.facebook.com/"><i className="fa fa-facebook" aria-hidden="true"></i></a>
+			<a href="https://www.twitter.com/"><i className="fa fa-twitter" aria-hidden="true"></i> Twitter</a>
 		</span>
 		</div>
 	</div>
 	
 	
-		<div class="right">
+		<div className="right">
 		<h1>Register</h1>
-		<p>Already have an account? <a href="#">Log in now</a></p>
-		<div class="inputs">
+		<p>Already have an account? <a href="/student-login">Log in now</a></p>
+		<div className="inputs">
        
         Name:
    
     <input type="text" id="name" name="name"/>
-<span style="color:red"></span><br/>
+<span ></span><br/>
        
 Email:   
     <input type="email" id="email" name="email"/>
-    <span style="color:red"></span><br/>
+    <span ></span><br/>
 
    
         Phone No:
    
     <input type="text" id="phn" name="phn"/>
-    <span style="color:red"></span><br/>
+    <span ></span><br/>
 
         Password:
  
     <input type="text" id="pass" name="pass"/>
-    <span style="color:red"></span><br/>
+    <span ></span><br/>
 
         Current Job:
    
     <input type="text" id="cj" name="cj"/>
-    <span style="color:red"></span><br/>
+    <span ></span><br/>
 
         CV:
    
     <input type="file" id="cv" name="cv" accept=".pdf/"/>
-    <span style="color:red"></span>
+    <span ></span>
 <br/>
 
         Photo:
 
     <input type="file" id="photo" name="photo" accept="image/*"/>
-    <span style="color:red"></span><br/>
+    <span ></span><br/>
 
         DoB:
    
     <input type="date" id="dob" name="dob "/>
-    <span style="color:red"></span><br/>
+    <span ></span><br/>
 		</div>
 		
 			
@@ -82,7 +85,7 @@ Email:
 	</div>
 	
 </div>
-
+</div>
 </body>
 </html>
      );
